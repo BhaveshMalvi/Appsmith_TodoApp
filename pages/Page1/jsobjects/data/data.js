3a399da2a4491c8b606cc8074de9aa18f5d48a98
genaxih754 <genaxih754@aleitar.com>
2024-11-04 13:28:58 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/data/data.js
@@ -0,0 +1,9 @@
+export default {
+	todos: undefined,
+	activeTodo: undefined,
+	dummyTodos: [
+		{title: ' add todo title 1', completed: true},
+		{title: ' add todo title 2', completed: false},
+		{title: ' add todo title 3', completed: true},
+	]
+}
